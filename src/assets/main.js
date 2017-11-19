db7f06e4abfc0292a71c0b30c25e3061fd08f7a1
Eliz964 <eliz964@hotmail.com>
2017-11-19 17:40:36 -0600

    Working version

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,7 +4,7 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if(answer == '' || attempt == '') {
+    if(answer.value == '' || attempt.value == '') {
     	setHiddenFields();
     }
 
@@ -28,7 +28,7 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields() {
-	answer.value = Math.round(Math.random()*10000).toString();
+	answer.value = Math.floor(Math.random()*10000).toString();
 	while(answer.value.length < 4) {
 		answer.value = "0" + answer.value;
 	}
