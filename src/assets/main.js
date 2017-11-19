09915418a95124dbd7414f67426804138bd88e7f
Eliz964 <eliz964@hotmail.com>
2017-11-19 16:49:45 -0600

    Updated js

--- src/assets/main.js
+++ src/assets/main.js
@@ -10,7 +10,14 @@ function guess() {
     if(!validateInput(input.value)) {
     	return false;
     } else {
-    	attempt += 1;
+    	attempt.value += 1;
+    }
+    if(getResults(inupt.value)) {
+    	setMessage("You Win! :)");
+    } else if(attempt >= 10) {
+    	setMessage("You Lose! :(");
+    } else {
+    	setMessage("Incorrect, try again.");
     }
 }
 
@@ -20,18 +27,48 @@ function setHiddenFields() {
 	while(answer.value.length < 4) {
 		answer.value = "0" + answer.value;
 	}
-	attempt = 0;
+	attempt = "0";
 }
 
-function setMessage(input) {
-	message.innerHTML = input;
+function setMessage(message) {
+	document.getElementById('message').innerHTML = message;
 }
 
-function validateInput(parameter1) {
-	if (parameter1.length === 4) {
+function validateInput(input) {
+	if (input.length === 4) {
 		return true;
 	} else {
 		setMessage("Guesses must be exactly 4 characters long.")
 		return false;
 	}
+}
+
+function getResults(input) {
+	let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+	for(i = 0; i < input.length; i++) {
+		if(input.charAt(i) === answer.value.charAt(i)) {
+			html += '<span class="glyphicon glyphicon-ok"></span>';
+		} else if(answer.value.indexOf(input.charAt(i)) > -1) {
+			html += '<span class="glyphicon glyphicon-transfer"></span>';
+		} else {
+			html += '<span class="glyphicon glyphicon-remove"></span>';
+		}
+	}
+	html += '</div></div>';
+	documnet.getElementById('results').innerHTML += html;
+
+	if(answer.value == input) {
+		return true;
+	}
+	return false;
+}
+
+function showAnswer(success) {
+	let code = document.getElementById('code')
+	if(success) {
+		code.className += 'success';
+	} else {
+		code.className += 'failure';
+	}
+	code.innerHTML = answer.value;
 }
